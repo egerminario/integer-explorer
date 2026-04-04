2831775f8b117547478ef1295cc0b04a811cc5fb
Erika Germinario <erika.germinario@gmail.com>
2026-04-04 16:39:38 -0500

    Step 1!

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+def valid_4bit(bits):
+    return len(bits) == 4 and all(b in "01" for b in bits)
 
+a = input("Enter 4-bit binary number A: ")
+b = input("Enter 4-bit binary number B: ")
+
+while not valid_4bit(a):
+	a = input("Invalid input. Enter exactly 4 bits for A: ")
+while not valid_4bit(b):
+	b = input("Invalid input. Enter exactly 4 bits for B: ")
+
+print("A=", a)
+print("B=", b)
